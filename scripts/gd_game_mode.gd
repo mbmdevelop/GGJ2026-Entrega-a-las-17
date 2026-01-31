891f421837db53abf3e620cd146a909f2d0f0e28
Rubén Sánchez <rubism3@gmail.com>
2026-01-31 14:58:15 +0100

    Dialogic implementado

--- scripts/gd_game_mode.gd
+++ scripts/gd_game_mode.gd
@@ -3,6 +3,7 @@ extends Node
 @export var waves_times:Array[float]
 
 @onready var wave_timer: Timer = $WaveTimer
+@onready var spawn_timer: Timer = $SpawnTimer
 @onready var hud: Hud = $"../Hud"
 @onready var current_wave_lable: Label = $"../Hud/CurrentWaveLable"
 
@@ -17,7 +18,19 @@ func _process(_delta: float) -> void:
 	print(wave_timer.time_left)
 	hud.set_countdowntime(wave_timer.time_left)
 	hud.set_currentwave(current_wave_num)
-	if wave_timer.is_stopped():
-		current_wave_num += 1
-		wave_timer.wait_time = waves_times[current_wave_num]
-		wave_timer.start()
+
+
+func start_next_wave(stop_dialogue:bool):
+	current_wave_num += 1
+	if current_wave_num >= waves_times.size():
+		return
+	if stop_dialogue:
+		get_tree().set('paused', false)
+	wave_timer.wait_time = waves_times[current_wave_num]
+	wave_timer.start()
+		
+func _on_wave_timer_timeout() -> void:
+	Dialogic.start("res://timelines/tm_dialogo1.dtl").process_mode = Node.PROCESS_MODE_ALWAYS
+	Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
+	Dialogic.timeline_ended.connect(start_next_wave.bind(true))
+	get_tree().paused = true
